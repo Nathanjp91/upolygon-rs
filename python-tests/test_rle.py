5bf1e09877d3696009bb28548d8a9415ba5b1f15
Nathan Perkins <nathanjp91@gmail.com>
2023-06-13 23:48:28 +0100

    2d rle

--- python-tests/test_rle.py
+++ python-tests/test_rle.py
@@ -3,3 +3,13 @@ import pytest
 import unittest
 import numpy as np
 
+def test_rle():
+    data = np.array([0, 0, 0, 1, 1, 1, 1, 0, 0, 0])
+    output = up.rle_encode(data)
+    assert output == [0, 3, 1, 4, 0, 3]
+    
+
+def test_rle_multi():
+    data = np.array([[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],[0, 0, 0, 1, 1, 1, 1, 0, 0, 0]])
+    output = up.rle_encode(data)
+    assert output == [0, 3, 1, 4, 0, 3]
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,40 @@
 use pyo3::prelude::*;
 use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
 
+#[derive(FromPyObject)]
+enum Mask {
+    SingleD(Vec<u64>),
+    MultiD(Vec<Vec<u64>>),
+} 
 
 #[pyfunction]
-fn rle_encode(data: Vec<Vec<u8>>) -> PyResult<Vec<u8>> {
+fn rle_encode(data: Mask) -> PyResult<Vec<u64>> {
+    match data {
+        Mask::SingleD(data) => return Ok(rle_encode_1d(data)),
+        Mask::MultiD(data) => return Ok(rle_encode_2d(data)),
+    }
+}
+
+fn rle_encode_1d(data: Vec<u64>) -> Vec<u64> {
+    let mut encoded = Vec::new();
+    let mut count = 0;
+    let mut prev = data[0];
+    for col in data {
+        if col == prev {
+            count += 1;
+        } else {
+            encoded.push(count);
+            encoded.push(prev);
+            count = 1;
+            prev = col;
+        }
+    }
+    encoded.push(count);
+    encoded.push(prev);
+    encoded
+}
+
+fn rle_encode_2d(data: Vec<Vec<u64>>) -> Vec<u64> {
     let mut encoded = Vec::new();
     let mut count = 0;
     let mut prev = data[0][0];
@@ -21,7 +52,7 @@ fn rle_encode(data: Vec<Vec<u8>>) -> PyResult<Vec<u8>> {
     }
     encoded.push(count);
     encoded.push(prev);
-    Ok(encoded)
+    encoded
 }
 
 /// A Python module implemented in Rust.
