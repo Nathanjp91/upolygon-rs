8c12224c082e37c52a436ef445efc7cb2b216f18
Nathan Perkins <nathanjp91@gmail.com>
2023-06-13 23:52:51 +0100

    working tests

--- python-tests/test_rle.py
+++ python-tests/test_rle.py
@@ -6,10 +6,10 @@ import numpy as np
 def test_rle():
     data = np.array([0, 0, 0, 1, 1, 1, 1, 0, 0, 0])
     output = up.rle_encode(data)
-    assert output == [0, 3, 1, 4, 0, 3]
+    assert output == [3, 0, 4, 1, 3, 0]
     
 
 def test_rle_multi():
     data = np.array([[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],[0, 0, 0, 1, 1, 1, 1, 0, 0, 0]])
     output = up.rle_encode(data)
-    assert output == [0, 3, 1, 4, 0, 3]
+    assert output == [3, 0, 4, 1, 6, 0, 4, 1, 3, 0]
